b2db7f6c9f6b0a2f1c91d355d7ed300f9a9267d9
Gyana <biswal4553@gmail.com>
2024-11-11 13:54:41 +0530

    updated changes

--- ec2-vpc-nginx.tf
+++ ec2-vpc-nginx.tf
@@ -10,29 +10,84 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_security_group" "gyana" {
-  name        = "Gyana_sg"
-  description = "Allow SSH and HTTP traffic"
+#Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "my_vpc"
+  }
+}
 
+#Private subnet
+resource "aws_subnet" "private-subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.my_vpc.id
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+#Public subnet
+resource "aws_subnet" "public-subnet" {
+  cidr_block              = "10.0.2.0/24"
+  vpc_id                  = aws_vpc.my_vpc.id
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+#Internet gateway
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+#Routing table
+resource "aws_route_table" "my-rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-igw.id
+  }
+}
+
+resource "aws_route_table_association" "public-sub" {
+  route_table_id = aws_route_table.my-rt.id
+  subnet_id      = aws_subnet.public-subnet.id
+}
+resource "aws_security_group" "nginx-sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  #Inbound rule for HTTP
+  
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  
   }
-
+  
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  #Outbound rule
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"  # Allow all outbound traffic
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nginx-sg"
   }
 }
 
